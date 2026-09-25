e910a120dc961dd86198615a9acefeee97b07288
Nagaraju1234-art <nagarajucivillbrce123@gmail.com>
2026-09-25 09:04:23 +0530

    Update backend.tf

--- EKS-Terraform/backend.tf
+++ EKS-Terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "devncloudtechdevopsss"
+    bucket       = "nagarajudevcloud1463457"
     key          = "terraform.tfstate"
     region       = "us-east-1"
     use_lockfile = true
